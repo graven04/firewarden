5bcfd3237779f06bfa525384068e1264b10ef1d3
graven04 <93924539+graven04@users.noreply.github.com>
2024-06-26 19:21:05 +0100

    Add the command line functionality using the clingon library
    Added the required code using the clingon library to implement command line functionality.
    cli/options provides the command line arguments and flags for the program.
    cli/command is the main function that defines the program it the command line
    cli/handler is the handler function needed by the cli/command function in the clingon library
    main is the entrypoint for the program

--- src/main.lisp
+++ src/main.lisp
@@ -2,4 +2,52 @@
   (:use :cl))
 (in-package :firewarden)
 
-;; blah blah blah.
+;; load the relevent libraries
+(ql:quickload "clingon") ; load clingon
+(ql:quickload "cl-csv") ; load cl-csv 
+
+
+;; Create the command line feature fo the program with flags and arguments using clingon library
+
+(defun cli/options ()
+  "Returns a list of options for our main command"
+  (list
+   (clingon:make-option
+    :filepath
+    :description "firefox passwords csv file"
+    :short-name #\f
+    :long-name "firefox"
+    :key :firefox)
+   (clingon:make-option
+    :filepath
+    :description "bitwarden passwords csv file"
+    :short-name #\b
+    :long-name "bitwarden"
+    :key :bitwarden)
+  (clingon:make-option
+   :flag      
+   :description "short --help"
+   :short-name #\h
+   :key :help)))
+
+
+(defun cli/command ()
+  "A command to sync firefox passwords to bitwardern using exported csv files"
+  (clingon:make-command
+   :name "firewarden"
+   :description "A command to sync firefox passwords to bitwardern using exported csv files"
+   :version "0.1"
+   :options (cli/options)
+   :handler (cli/handler)))
+
+
+(defun cli/handler (cmd)
+  "A handler function for cli/command"
+  (defvar *firefox-csv-path* (clingon:getopt cmd :firefox)
+    (defvar *firefox-csv-path* (clingon:getopt cmd :firefox))))
+
+
+(defun main ()
+  "The main entrypoint of the firewarden program"
+  (let ((app (cli/command)))
+    (clingon:run app)))
